a264e886b0e6ed09ad6f537f8ff2bbb60e7cac79
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-11-02 22:22:12 +0200

    MP-2: fixed minor errors

--- client/mealpler/Mealpler.js
+++ client/mealpler/Mealpler.js
@@ -1,4 +1,4 @@
-let Mealpler = angular.module('Mealpler', ['ngRoute', 'requestSrvc', 'ui.bootstrap', 'ui.bootstrap.tpls']);
+let Mealpler = angular.module('Mealpler', ['ngAnimate', 'ngRoute', 'requestSrvc', 'ui.bootstrap', 'ui.bootstrap.tpls']);
 Mealpler.config(function ($routeProvider) {
     $routeProvider
         .when('/',
