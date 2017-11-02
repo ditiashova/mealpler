3410dd905910d7b202ae40bcc65588f17f251301
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-11-02 11:28:29 +0200

    MP-2: added date range picker via cdn

--- client/mealpler/dashboard/week/WeekCtrl.js
+++ client/mealpler/dashboard/week/WeekCtrl.js
@@ -1,4 +1,18 @@
 Mealpler.controller('WeekCtrl', function (WeekModel) {
+    const datePicker = $('input[name="daterange"]');
     let week = this;
     week.days = WeekModel.weekDays();
+    //settings for Date Range Picker
+    datePicker.daterangepicker({
+        "dateLimit": {
+            "days": 7
+        },
+        "startDate": "10/26/2017",
+        "endDate": "11/01/2017"
+    }, function(start, end, label) {
+        console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+    });
+    datePicker.on('apply.daterangepicker', function (e, picker) {
+        // change for event with choosing new range
+    })
 });
