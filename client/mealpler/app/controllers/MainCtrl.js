0cdd204bbfd657c68bd1ca3a55227eeba7a629d4
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-11-28 16:00:00 +0200

    MP-18: sidebar appears on left with toggleFridge function

--- client/mealpler/app/controllers/MainCtrl.js
+++ client/mealpler/app/controllers/MainCtrl.js
@@ -1,4 +1,9 @@
 Mealpler.controller('MainCtrl', ['request', function (request) {
     let main = this;
     main.title = Mealpler.titles;
+    main.openedFridge = false;
+
+    main.toggleFridge = function () {
+        main.openedFridge = !main.openedFridge;
+    };
 }]);
