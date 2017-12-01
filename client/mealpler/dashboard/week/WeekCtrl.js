ad7ffafcb45c4fb6c2f0bdec72f8f173cfb4244f
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-01 23:45:43 +0200

    MP-25: extracted day functionality to DayCtrl

--- client/mealpler/dashboard/week/WeekCtrl.js
+++ client/mealpler/dashboard/week/WeekCtrl.js
@@ -5,7 +5,7 @@ function WeekController ($rootScope, $scope, WeekModel, MealModel, StorageModel)
     const datePicker = $('input[name="datepicker"]');
 
     this.day = {}; // here goes all methods for the day events
-    this.activeTab = 'product';
+    /*this.activeTab = 'product';*/
     this.today = moment().format('YYYY-M-D');
     this._weekDuration = 7;
 
@@ -24,82 +24,6 @@ function WeekController ($rootScope, $scope, WeekModel, MealModel, StorageModel)
         $scope.$apply();
     });
 
-    //day settings
-    this.day.setCurrentMeal = (meal, date) => {
-        this.day.refreshCurrentMeal();
-        this.currentMeal = angular.copy(meal);
-        this.currentDate = date;
-        this.day.createNewProduct(this.newProducts);
-        this.day.createNewProduct(this.newRecipe);
-    };
-
-    this.day.addNewItems = (type, forMeal, forDay, newItems) => {
-        if (type === 'list') {
-            forMeal.mealList = forMeal.mealList.concat(newItems.list);
-        } else if (type === 'recipe') {
-            forMeal.mealList.push(newItems);
-        } else if (type === 'stored') {
-            forMeal.mealList = forMeal.mealList.concat(newItems.mealList);
-        }
-        MealModel.saveMealInfo(forMeal,forDay);
-        this.day.refreshCurrentMeal();
-        this._loadMealsDataForWeek();
-    };
-
-    this.day.deleteMeal = (meal, date) => {
-        MealModel.deleteAllMeal(meal, date);
-        this.day.refreshCurrentMeal();
-        this._loadMealsDataForWeek();
-    };
-
-    this.day.deleteItem = (item, mealName, date) => {
-        MealModel.deleteItemMeal(item, mealName, date);
-        this._loadMealsDataForWeek();
-    };
-
-    this.day.deleteIngredient = (item, recipe, mealName, date) => {
-        MealModel.deleteIngredient(item, recipe, mealName, date);
-        this._loadMealsDataForWeek();
-    };
-
-    this.day.createNewProduct = (forMeal) => {
-        forMeal.list.push(angular.copy(MealModel.createDefaultProduct()));
-    };
-
-    this.day.refreshCurrentMeal = () => {
-        this.currentMeal = {}; this.currentDate = '';
-        this.day.refreshNewItems();
-    };
-
-    this.day.refreshNewItems = () => {
-        this.newProducts = {};
-        this.newProducts.list = [];
-        this.newRecipe = angular.copy(MealModel.createDefaultRecipe());
-    };
-
-    this.day.copyFood = (name, content) => {
-        StorageModel.addFoodToStored(name, content);
-    };
-
-    this.day.pasteMenu = (forDay) => {
-        let stored = StorageModel.getStoredItem("menu");
-        let dat = MealModel.createNewDay(forDay);
-        stored.forEach(a => dat.mealsList.push(a));
-        MealModel.updateMealsList(forDay.format("YYYY-M-D"), dat);
-        this._loadMealsDataForWeek();
-    };
-
-    this.day.pasteFood = (name, forMeal, forDay) => {
-        let stored = StorageModel.getStoredItem(name);
-        this.day.addNewItems('stored', forMeal, forDay, stored);
-    };
-
-    this.day.mealModal = (tab) => {
-        this.activeTab = tab;
-        //this.day.addProduct = tab === 0;
-        //this.day.addRecipe = !this.day.addProduct;
-    };
-
     this.switchWeek = (time) => {
         if (time === 'past') {
             this.init(this.range[0].subtract(1, 'day'));
@@ -113,7 +37,8 @@ function WeekController ($rootScope, $scope, WeekModel, MealModel, StorageModel)
         this._calculateWeekRange(moment(forDate).startOf('week'));
         //get meal's list for each day of week range
         this._loadMealsDataForWeek();
-        this.day.refreshCurrentMeal();
+        $rootScope.$broadcast('refreshCurrentMeal');
+        //this.day.refreshCurrentMeal(); //broadcast event to refresh in dayCtrl
     };
 
 
@@ -140,4 +65,7 @@ function WeekController ($rootScope, $scope, WeekModel, MealModel, StorageModel)
     };
 
     this.init(today);
+
+    $rootScope.$on('refreshDataForWeek', () => this._loadMealsDataForWeek());
+
 }
--- client/mealpler/dashboard/week/day/DayCtrl.js
+++ client/mealpler/dashboard/week/day/DayCtrl.js
@@ -1,7 +1,101 @@
-Mealpler.controller('DayCtrl', function (MealModel) {
-    let day = this;
+Mealpler.controller('DayCtrl', DayCtrl);
+
+function DayCtrl ($rootScope, $scope, MealModel, StorageModel) {
+    this.activeTab = 'product';
+
+    this.copyFood = (name, content) => {
+        StorageModel.addFoodToStored(name, content);
+    };
+
+    this.deleteItem = (item, mealName, date) => {
+        MealModel.deleteItemMeal(item, mealName, date);
+        $rootScope.$broadcast('refreshDataForWeek');
+        //this._loadMealsDataForWeek(); //emit event for updating week data
+    };
+
+
+
+    this.createNewProduct = (forMeal) => {
+        forMeal.list.push(angular.copy(MealModel.createDefaultProduct()));
+    };
+
+    this.refreshNewItems = () => {
+        this.newProducts = {};
+        this.newProducts.list = [];
+        this.newProducts.list.push(angular.copy(MealModel.createDefaultProduct()));
+        this.newRecipe = angular.copy(MealModel.createDefaultRecipe());
+        this.newRecipe.list.push(angular.copy(MealModel.createDefaultProduct()));
+    };
+
+    this.refreshCurrentMeal = () => {
+        this.currentMeal = {}; this.currentDate = '';
+    };
+
+    this.setCurrentMeal = (meal, date) => { //when modal opens
+        this.refreshCurrentMeal();
+        this.refreshNewItems();
+        this.currentMeal = angular.copy(meal);
+        this.currentDate = date;
+        //this.createNewProduct(this.newProducts);
+        //this.createNewProduct(this.newRecipe);
+        console.log(this.newProducts.list);
+    };
+
+    this.deleteIngredient = (item, recipe, mealName, date) => {
+        MealModel.deleteIngredient(item, recipe, mealName, date);
+        $rootScope.$broadcast('refreshDataForWeek');
+        //this._loadMealsDataForWeek(); //emit event for updating week data
+    };
+
+    this.deleteMeal = (meal, date) => {
+        MealModel.deleteAllMeal(meal, date);
+        this.refreshCurrentMeal();
+        $rootScope.$broadcast('refreshDataForWeek');
+        //this._loadMealsDataForWeek();
+    };
+
+    this.addNewItems = (type, forMeal, forDay, newItems) => {
+        if (type === 'list') {
+            forMeal.mealList = forMeal.mealList.concat(newItems.list);
+        } else if (type === 'recipe') {
+            forMeal.mealList.push(newItems);
+        } else if (type === 'stored') {
+            forMeal.mealList = forMeal.mealList.concat(newItems.mealList);
+        }
+        MealModel.saveMealInfo(forMeal,forDay);
+        this.refreshCurrentMeal();
+        $rootScope.$broadcast('refreshDataForWeek');
+        //this._loadMealsDataForWeek(); //emit event for updating week data
+    };
+
+    this.pasteFood = (name, forMeal, forDay) => {
+        let stored = StorageModel.getStoredItem(name);
+        this.addNewItems('stored', forMeal, forDay, stored);
+    };
+
+    this.pasteMenu = (forDay) => {
+        let stored = StorageModel.getStoredItem("menu");
+        let dat = MealModel.createNewDay(forDay);
+        stored.forEach(a => dat.mealsList.push(a));
+        MealModel.updateMealsList(forDay.format("YYYY-M-D"), dat);
+        $rootScope.$broadcast('refreshDataForWeek');
+        //this._loadMealsDataForWeek(); //emit event for updating week data
+    };
+
+    this.setMealModal = (tab) => {
+        this.activeTab = tab;
+    };
+
+    this.refreshNewItems();
+
+    $rootScope.$on("refreshCurrentMeal", () => this.refreshCurrentMeal());
+
+
+
+
+    /*let day = this;
     let defaultProduct = {
-        /*"id": Math.random(),*/
+        /!*"id": Math.random(),*!/
         "name": "",
         "type": "product",
         "quantity": 1,
@@ -36,5 +130,5 @@ Mealpler.controller('DayCtrl', function (MealModel) {
         day.mealsList = list != null ? list : angular.copy(MealModel.emptyMealsList());
     };
 
-    day.init();
-});
+    day.init();*/
+}
