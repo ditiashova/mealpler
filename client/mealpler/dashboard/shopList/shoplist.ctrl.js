e315c9c0570a9024e0bd9042a807d4a66e003c2a
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-02 23:04:30 +0200

    MP-25: created ShoplistCtrl with functionality for SL

---
+++ client/mealpler/dashboard/shopList/shoplist.ctrl.js
@@ -0,0 +1,65 @@
+Mealpler.controller('ShoplistCtrl', ShoplistCtrl);
+
+function ShoplistCtrl ($rootScope, $scope, StorageModel) {
+    const today = moment();
+    const dateRangePicker = $("#dateRangePicker");
+    const localization = {
+        "format": "DD/MM/YYYY",
+        "firstDay": 1
+    };
+
+    this.rangeStart = today.startOf('week');
+    this.rangeLength = 7;
+
+    //settings for Date Range Picker
+    dateRangePicker.daterangepicker({
+        "locale": localization,
+        "dateLimit": {
+            "days": 14
+        },
+        "showDropdowns": true,
+        "startDate": this.defaultRangeStart
+    }, (start, end, label) => {
+        this.rangeStart = start;
+        this.rangeLength = end.diff(start, 'days')+1;
+        this.init(this.rangeStart, this.rangeLength);
+        $scope.$apply();
+    });
+
+    this.addItem = (newItem) => {
+        StorageModel.addItemToGroceryList(newItem);
+        this.init();
+    };
+
+    this.addItems = (listOfMeals) => {
+        let newItems = [];
+        listOfMeals.map(a => a.mealList.forEach(b => b.type === 'product' ? newItems.push(b) : b.list.forEach(c => newItems.push(c))));
+        StorageModel.addItemToGroceryList(newItems);
+        this.grocery.init();
+    };
+
+    this.deleteItem = (item) => {
+        StorageModel.deleteGroceryItem(item);
+        this.grocery.init();
+    };
+
+    this.deleteAll = () => {
+        StorageModel.deleteGrocery();
+        this.grocery.init();
+    };
+
+    this.init = (start, duration) => {
+        if (!start || !duration) {
+            start = this.rangeStart;
+            duration = this.rangeLength;
+        }
+        const storedItems = MealModel.findDateRangeMealList(start, duration);
+        this.list = MealModel.extractAndSortProducts(storedItems);
+    };
+
+
+    $scope.$on('updateShopList', (e, date) => {
+        this.init(date, this.rangeLength);
+    });
+
+}
