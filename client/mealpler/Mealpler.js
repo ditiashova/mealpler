68abc3522d8a6ef46dac913ea4accedd1bd47296
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-02 18:33:23 +0200

    MP-26: added localization to change first day of week on Monday

--- client/mealpler/Mealpler.js
+++ client/mealpler/Mealpler.js
@@ -1,11 +1,21 @@
 let Mealpler = angular.module('Mealpler', ['ngAnimate', 'ngRoute', 'requestSrvc', 'ui.bootstrap', 'ui.bootstrap.tpls']);
-Mealpler.config(function ($routeProvider) {
-    $routeProvider
-        .when('/',
-            {
-                templateUrl: 'scripts/dashboard/dashboard.html',
-                controller: 'DashboardCtrl',
-                controllerAs: 'dashboard'
-            })
-        .otherwise({redirectTo: '/'});
+Mealpler.
+    config(function ($routeProvider) {
+        $routeProvider
+            .when('/',
+                {
+                    templateUrl: 'scripts/dashboard/dashboard.html',
+                    controller: 'DashboardCtrl',
+                    controllerAs: 'dashboard'
+                })
+            .otherwise({redirectTo: '/'});
+}).
+    config(function() {
+        moment.locale('dow', {
+            week : {
+                dow: 1,
+            }
+        });
+        moment.locale('dow');
 });
+
