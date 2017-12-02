905453c9cc0745d927f48fc793a2cfe9a0c66962
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-02 21:28:43 +0200

    MP-25: refactoring for date range picking

--- client/mealpler/dashboard/DashboardCtrl.js
+++ client/mealpler/dashboard/DashboardCtrl.js
@@ -1,26 +1,79 @@
 Mealpler.controller('DashboardCtrl', DashboardController);
 
 function DashboardController ($rootScope, $scope, MealModel, StorageModel) {
-    const dateRangePicker = $('input[name="daterangepicker"]');
+    const dateRangePicker = $("#dateRangePicker");
+    const datePicker = $("#datePicker");
+    const localization = {
+        "format": "DD/MM/YYYY",
+        "firstDay": 1
+    };
+    const today = moment();
+
     this.grocery = {};
-    this.defaultRangeStart = moment().startOf('week');
+
+    this.weekStartDate = today.startOf('week');
+    this.weekLength = 7;
+
+    this.defaultRangeStart = this.weekStartDate;
     this.defaultRangeLength = 7;
 
     //settings for Date Range Picker
     dateRangePicker.daterangepicker({
+        "locale": localization,
         "dateLimit": {
             "days": 14
         },
         "showDropdowns": true,
-        "startDate": moment().startOf('week')
-    }, () => {});
-
-    dateRangePicker.on('apply.daterangepicker', (e, picker) => {
-        this.defaultRangeStart = picker.startDate;
-        this.defaultRangeLength = picker.endDate.diff(picker.startDate, 'days')+1;
+        "startDate": this.defaultRangeStart
+    }, (start, end, label) => {
+        this.defaultRangeStart = start;
+        this.defaultRangeLength = end.diff(start, 'days')+1;
         this.grocery.init();
         $scope.$apply();
     });
+
+    /*dateRangePicker.on('apply.daterangepicker', (e, picker) => {
+    });*/
+
+    //settings for Date Picker
+    datePicker.daterangepicker({
+        "locale": localization,
+        "singleDatePicker": true,
+        "showDropdowns": true,
+        "startDate": this.weekStartDate
+    }, (start, end, label) => {
+        this.weekStartDate = start;
+        this.defaultRangeStart = this.weekStartDate;
+        $scope.$broadcast('refreshDataForWeek', this.weekStartDate);
+        $scope.$apply();
+    });
+
+    /*datePicker.on('apply.daterangepicker', (e, picker) => {
+        this.weekStartDate = picker.startDate;
+        this.defaultRangeStart = this.weekStartDate;
+        $scope.$broadcast('refreshDataForWeek', this.weekStartDate);
+        //this.init(newStartDate);
+        $scope.$apply();
+    });*/
+
+    this.switchWeek = (time) => {
+        if (time === 'past') {
+            const newStartDate = this.weekStartDate.subtract(1, 'day');
+            this.weekStartDate = this.setNewWeekStart(newStartDate);
+            this.defaultRangeStart = this.weekStartDate;
+            $scope.$broadcast('refreshDataForWeek', newStartDate);
+        } else if (time === 'future') {
+            const newStartDate = this.weekStartDate.add(this.weekLength, 'day').add(1, 'day');
+            this.weekStartDate = this.setNewWeekStart(newStartDate);
+            this.defaultRangeStart = this.weekStartDate;
+            $scope.$broadcast('refreshDataForWeek', newStartDate);
+            //broadcast
+        }
+    };
+
+    this.setNewWeekStart = (randomDate) => {
+        return randomDate.startOf('week');
+    };
 
     this.grocery.addItem = (newItem) => {
         StorageModel.addItemToGroceryList(newItem);
