3ad3e5e171268fce0fa1c8783a5ad8b995bb99b6
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-04 19:27:08 +0200

    MP-29: expanded meal manager functionality

--- client/mealpler/dashboard/week/day/meal/meal.directive.js
+++ client/mealpler/dashboard/week/day/meal/meal.directive.js
@@ -1,16 +1,27 @@
 Mealpler.directive('mealManager', function () {
-    const link = (scope, el, attrs, controller) => {
+    const link = (scope, el, attrs, controllers) => {
+        const DayCtrl = controllers[0];
+        const WeekCtrl = controllers[1];
+        const MealCtrl = scope.meal;
+        scope.copyFood = (name, food) => {
+            DayCtrl.copyFood(name, food);
+        };
+        scope.pasteFood = (name, food, date) => {
+            MealCtrl.pasteFood(name, food, date);
+            WeekCtrl._loadMealsDataForWeek();
+        };
     };
 
     return {
         restrict: 'E',
         transclude: true,
         scope: {
-            food: '='
+            food: '=',
+            date: '='
         },
         controller: 'MealCtrl',
         controllerAs: 'meal',
-        require: '^^dayManager',
+        require: ['^^dayManager', '^^weekManager'],
         templateUrl: 'scripts/dashboard/week/day//meal/meal.tmpl.html',
         link: link
     };
