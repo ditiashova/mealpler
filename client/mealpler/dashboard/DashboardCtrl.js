5a6d139bf696df63b32339347216f324f0e9b3f6
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-04 14:59:40 +0200

    MP-29: cleaned DashboardCtrl

--- client/mealpler/dashboard/DashboardCtrl.js
+++ client/mealpler/dashboard/DashboardCtrl.js
@@ -1,7 +1,5 @@
 Mealpler.controller('DashboardCtrl', DashboardController);
 
 function DashboardController () {
-    const today = moment();
-    this.weekStartDate = today.startOf('week');
-    this.weekLength = 7;
+
 }
