ad33e888940ac13d2b24bf51b9bc0b0ef1871d8d
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-04 19:08:18 +0200

    MP-29: expanded portion block functionality

--- client/mealpler/dashboard/week/day/meal/portion/portion.directive.js
+++ client/mealpler/dashboard/week/day/meal/portion/portion.directive.js
@@ -1,16 +1,27 @@
-Mealpler.directive('portionBlock', function () {
-    const link = (scope, el, attrs, controller) => {
+Mealpler.directive('portionBlock', function (MealModel) {
+    const link = (scope, el, attrs, controllers) => {
+        const DayCtrl = controllers[1];
+        const WeekCtrl = controllers[2];
+        scope.deleteItem = (item, mealName, date) => {
+            MealModel.deleteItemMeal(item, mealName, date);
+            WeekCtrl._loadMealsDataForWeek();
+        };
+        scope.copyFood = (name, food) => {
+            DayCtrl.copyFood(name, food);
+        };
     };
 
     return {
         restrict: 'E',
         transclude: true,
         scope: {
-            item: '='
+            item: '=',
+            mealName: '=',
+            date: '='
         },
         controller: 'PortionCtrl',
         controllerAs: 'portion',
-        require: '^^mealManager',
+        require: ['^^mealManager', '^^dayManager', '^^weekManager'],
         templateUrl: 'scripts/dashboard/week/day/meal/portion/portion.tmpl.html',
         link: link
     };
