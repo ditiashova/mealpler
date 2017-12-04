6c2e0241a214790df67e9c6adfa224906421415c
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-04 17:55:22 +0200

    MP-29: added link function to communicate with MainCtrl

--- client/mealpler/dashboard/week/week.directive.js
+++ client/mealpler/dashboard/week/week.directive.js
@@ -1,10 +1,15 @@
 Mealpler.directive('weekManager', function () {
+    const link = (scope, el, attrs, controller) => {
+        //here we accept variables from MainCtrl
+    };
+
     return {
         restrict: 'E',
         transclude: true,
         controller: 'WeekCtrl',
         controllerAs: 'week',
         scope: {},
-        templateUrl: 'scripts/dashboard/week/week.tmpl.html'
+        templateUrl: 'scripts/dashboard/week/week.tmpl.html',
+        link: link
     };
 });
