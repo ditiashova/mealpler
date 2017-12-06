c581e89a0cf7ccaf1d02d846c88e36c8e38a37fa
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-06 20:51:53 +0200

    MP-29: improved addModal directive

--- client/mealpler/tmpl/modals/meal/add.directive.js
+++ client/mealpler/tmpl/modals/meal/add.directive.js
@@ -1,15 +1,24 @@
 Mealpler.directive('addModal', function () {
-    const link = (scope, el, attrs, controller) => {
-        const addMeal = scope.addMeal;
-        const WeekCtrl = controller[0];
-
+    const link = (scope, el, attrs, controllers) => {
+        const WeekCtrl = controllers[0];
+        const MealCtrl = controllers[1];
+        const day = attrs.date;
+        const meal = attrs.meal;
+        let s = scope;
+        scope.addMeal.saveNew = (type, newItems) => {
+            MealCtrl.addNewItems(type, newItems, meal, day);
+            WeekCtrl._loadMealsDataForWeek();
+        }
     };
 
     return {
         restrict: 'E',
         transclude: true,
-        scope: {},
-        require: ['^^weekManager'],
+        scope: {
+            date: '=',
+            meal: '='
+        },
+        require: ['^^weekManager', '^^mealManager'],
         controller: 'AddMealModalCtrl',
         controllerAs: 'addMeal',
         templateUrl: 'scripts/tmpl/modals/meal/add.modal.html',
