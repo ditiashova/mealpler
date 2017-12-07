854b77bd25f8d147437a9e63b104dd791ee1a10e
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-07 18:23:50 +0200

    MP-29: implemented delete modal and directive

---
+++ client/mealpler/tmpl/modals/meal/delete/delete.directive.js
@@ -0,0 +1,27 @@
+Mealpler.directive('deleteModal', function () {
+    const link = (scope, el, attrs, controllers) => {
+        const WeekCtrl = controllers[0];
+        const MealCtrl = scope.meal;
+        const date = MealCtrl.date;
+        const meal = MealCtrl.meal;
+        scope.confirmDelete = () => {
+            MealCtrl.delete(meal, moment(date));
+            WeekCtrl._loadMealsDataForWeek();
+        };
+        scope.cancel = () => { //for cancel icon in modal
+            MealCtrl.cancel();
+        };
+    };
+
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            date: '=',
+            meal: '='
+        },
+        require: ['^^weekManager', '^^mealManager'],
+        templateUrl: 'scripts/tmpl/modals/meal/delete/delete.modal.html',
+        link: link
+    };
+});
