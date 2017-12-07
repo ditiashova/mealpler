2d818885ef7d8d5b76b3abcd0702d3e0742836bd
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-07 12:20:47 +0200

    MP-29: minor js edits

--- client/mealpler/dashboard/week/day/meal/MealCtrl.js
+++ client/mealpler/dashboard/week/day/meal/MealCtrl.js
@@ -1,9 +1,10 @@
 Mealpler.controller('MealCtrl', MealController);
 
-function MealController ($scope, MealModel, StorageModel) {
+function MealController ($scope, MealModel, StorageModel, openModal, $document) {
 
     this.addNewItems = (type, newItems, forMeal, forDay) => {
-        let oldMealList = MealModel.findMealList(forDay).filter(a => a.mealName === forMeal)[0];
+        const formattedDay = moment(forDay).format("YYYY-M-D"); //in case nonformatted day was passed
+        let oldMealList = MealModel.findMealList(formattedDay).filter(a => a.mealName === forMeal)[0];
         if (type === 'list') {
             oldMealList.mealList = oldMealList.mealList.concat(newItems.list);
         } else if (type === 'recipe') {
@@ -11,11 +12,17 @@ function MealController ($scope, MealModel, StorageModel) {
         } else if (type === 'stored') {
             oldMealList.mealList = oldMealList.mealList.concat(newItems.mealList);
         }
-        MealModel.saveMealInfo(oldMealList,forDay);
+        MealModel.saveMealInfo(oldMealList,formattedDay);
     };
 
     this.pasteFood = (name, forMeal, forDay) => {
         let stored = StorageModel.getStoredItem(name);
-        this.addNewItems('stored', forMeal, forDay, stored);
+        this.addNewItems('stored', stored, forMeal, forDay);
     };
+
+    this.addNewMeal = (mealName, date) => {
+        const templateContent = '<add-modal date="' + date.format("YYYY-M-D") + '" meal="' + mealName + '"></add-modal>';
+        const parentDiv = angular.element($document[0].querySelector('.modal-parent'));
+        openModal.open(templateContent, parentDiv, $scope, controller);
+    }
 }
