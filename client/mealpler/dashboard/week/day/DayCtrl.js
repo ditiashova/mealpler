90b1609d0fddde08f148ce61a364e11d8ef88af0
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-08 11:53:23 +0200

    MP-29: cleaned DayCtrl

--- client/mealpler/dashboard/week/day/DayCtrl.js
+++ client/mealpler/dashboard/week/day/DayCtrl.js
@@ -1,45 +1,9 @@
 Mealpler.controller('DayCtrl', DayCtrl);
 
 function DayCtrl ($rootScope, $scope, MealModel, StorageModel) {
-    /*this.activeTab = 'product';*/
 
     this.copyFood = (name, content) => {
         StorageModel.addFoodToStored(name, content);
-    }; /*!!!*/
-
-    /*this.createNewProduct = (forMeal) => {
-        forMeal.list.push(angular.copy(MealModel.createDefaultProduct()));
-    };*/
-
-    this.refreshNewItems = () => {
-        this.newProducts = {};
-        this.newProducts.list = [];
-        this.newProducts.list.push(angular.copy(MealModel.createDefaultProduct()));
-        this.newRecipe = angular.copy(MealModel.createDefaultRecipe());
-        this.newRecipe.list.push(angular.copy(MealModel.createDefaultProduct()));
-    };
-
-    this.refreshCurrentMeal = () => {
-        this.currentMeal = {}; this.currentDate = '';
-    };
-
-    //when modal opens
-    /*this.setCurrentMeal = (meal, date) => {
-        this.refreshCurrentMeal();
-        this.refreshNewItems();
-        this.currentMeal = angular.copy(meal);
-        this.currentDate = date;
-    };*/
-
-    this.deleteIngredient = (item, recipe, mealName, date) => {
-        MealModel.deleteIngredient(item, recipe, mealName, date);
-        $rootScope.$broadcast('refreshMealsForWeek');
-    };
-
-    this.deleteMeal = (meal, date) => {
-        MealModel.deleteAllMeal(meal, date);
-        this.refreshCurrentMeal();
-        $rootScope.$broadcast('refreshMealsForWeek');
     };
 
     this.pasteMenu = (forDay) => {
@@ -47,14 +11,5 @@ function DayCtrl ($rootScope, $scope, MealModel, StorageModel) {
         let dat = MealModel.createNewDay(forDay);
         stored.forEach(a => dat.mealsList.push(a));
         MealModel.updateMealsList(forDay.format("YYYY-M-D"), dat);
-        $rootScope.$broadcast('refreshMealsForWeek');
     };
-
-    /*this.setMealModal = (tab) => {
-        this.activeTab = tab;
-    };*/
-
-    this.refreshNewItems();
-
-    $rootScope.$on("refreshCurrentMeal", () => this.refreshCurrentMeal());
 }
