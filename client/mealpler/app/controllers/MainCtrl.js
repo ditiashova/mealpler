ad8e114c0b8f5bf6aca2dd58f084dee33834ae52
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-12 17:10:11 +0200

    MP-29: refactored MainCtrl as directive to communicate with others

--- client/mealpler/app/controllers/MainCtrl.js
+++ client/mealpler/app/controllers/MainCtrl.js
@@ -1,9 +1,40 @@
 Mealpler.controller('MainCtrl', MainController);
+Mealpler.directive( 'mainBlock', function () {
+    const link = (scope, el, attrs, controller) => {
+        let a = scope.main;
+    };
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'scripts/tmpl/mainBlock.tmpl.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main',
+        link: link
+    };
+});
 
 function MainController () {
     this.title = Mealpler.titles;
-    this.openShopList = false;
-    this.toggleShopList = () => {
-        this.openShopList = !this.openShopList;
+    this.handlers = {
+        showShopList: []
     };
+    this.openedShopList = false;
+    this.toggleShopListState = () => {
+        this.openedShopList = !this.openedShopList;
+        this.executeShopListActions();
+    };
+    /*this.setShopListState = (bool) => {
+        this.openShopList = bool;
+    };*/
 }
+
+
+MainController.prototype.executeShopListActions = function () {
+    this.handlers.showShopList.forEach(function (handler) {
+        handler();
+    })
+};
+
+MainController.prototype.setShopListShowActions = function(handler) {
+    this.handlers.showShopList.push(handler);
+};
