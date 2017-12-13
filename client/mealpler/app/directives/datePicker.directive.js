732a136679c194a9b4df926fc816728766c613f0
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-13 17:11:13 +0200

    MP-29: added for datepicker directive new methods for shoplist daterangepicker

--- client/mealpler/app/directives/datePicker.directive.js
+++ client/mealpler/app/directives/datePicker.directive.js
@@ -1,19 +1,33 @@
 Mealpler.directive('datePicker', function () {
     const link = (scope, el, attrs, controller) => {
-        const weekCtrl = controller;
+        const dashboardCtrl = controller;
         const dateCtrl = scope.dateCtrl;
+        const startDate = dashboardCtrl.getWeekStartDate();
 
         dateCtrl.datePickerTarget = $('#' + attrs.name);
         dateCtrl.datePickerTarget.daterangepicker({
             "locale": dateCtrl.getLocalization(),
-            "singleDatePicker": !attrs.single,
+            "singleDatePicker": !!attrs.single,
             "showDropdowns": true,
-            "startDate": weekCtrl.weekStartDate
+            "startDate": startDate
         }, (start, end, label) => {
-            if (attrs.refreshFridge) {
 
+            //refresh week only from directive placed in week tmpl
+            if (!!attrs.refreshWeek) {
+                dashboardCtrl.refreshMealDataForWeek(start);
             }
-            weekCtrl.init(start);
+
+            //fridge needs to be refreshed in both date pickers, but this check is still needed in case I'll decide to remove this logic etc.
+            if (!!attrs.refreshFridge) {
+                if (!!attrs.single) {
+                    //if one date is chosen
+                    const startOfWeek = start.startOf('week');
+                    dashboardCtrl.refreshShopList(startOfWeek, dashboardCtrl.defaultWeekDuration);
+                } else {
+                    let duration = end.diff(start, 'days');
+                    dashboardCtrl.refreshShopList(start, duration);
+                }
+            }
             scope.$apply();
         });
 
@@ -26,9 +40,10 @@ Mealpler.directive('datePicker', function () {
         scope: {
             single: '=',
             refreshFridge: '=',
-            name: '='
+            name: '=',
+            refreshWeek: '='
         },
-        require: '^^weekManager',
+        require: '^^dashboard',
         controller: 'DatePickerCtrl',
         controllerAs: 'dateCtrl',
         link: link
