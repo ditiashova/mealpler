a0a5a2c903ff22e8971e351e2a3d09d1529f5569
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-13 17:12:28 +0200

    MP-29: communicate shoplist directive with dashboard

--- client/mealpler/dashboard/shopList/shoplist.directive.js
+++ client/mealpler/dashboard/shopList/shoplist.directive.js
@@ -1,6 +1,7 @@
 Mealpler.directive('shopList', function () {
-    const link = (scope, el, attrs, controller) => {
-        const mainCtrl = controller;
+    const link = (scope, el, attrs, controllers) => {
+        const mainCtrl = controllers[0];
+        const dashboardCtrl = controllers[1];
         const listCtrl = scope.listCtrl;
         //controller.showSideBar();
         listCtrl.closeShopList = () => {
@@ -8,13 +9,14 @@ Mealpler.directive('shopList', function () {
             //controller.showSideBar();
         };
 
+        dashboardCtrl.setShopListActions((date, duration) => listCtrl.init(date, duration));
     };
 
     return {
         restrict: 'E',
         transclude: true,
         scope: {},
-        require: '^^mainBlock',
+        require: ['^^mainBlock', '^^dashboard'],
         templateUrl: 'scripts/dashboard/shopList/shopList.tmpl.html',
         controller: 'ShoplistCtrl',
         controllerAs: 'listCtrl',
