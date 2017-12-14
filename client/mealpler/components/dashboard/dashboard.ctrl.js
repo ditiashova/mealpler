0533b9f920a439760594fba5bc231c8d2507d81c
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-14 16:45:07 +0200

    MP-34: rewrote DashboardCtrl as Class

--- client/mealpler/components/dashboard/dashboard.ctrl.js
+++ client/mealpler/components/dashboard/dashboard.ctrl.js
@@ -1,42 +1,44 @@
-Mealpler.controller('DashboardCtrl', DashboardController);
+class DashboardController {
+    constructor () {
+        this.defaultWeekDuration = 7;
+        this.defaultWeekStartDate = moment().startOf('week');
 
-function DashboardController () {
-    this.defaultWeekDuration = 7;
-    this.defaultWeekStartDate = moment().startOf('week');
+        this.handlers = {
+            shopListLoadingDataHandlers: [],
+            weekMealsLoadingDataHandlers: [],
+            datePickerHandlers: []
+        };
+    }
 
-    this.handlers = {
-        shopListLoadingDataHandlers: [],
-        weekMealsLoadingDataHandlers: [],
-        datePickerHandlers: []
-    };
-
-    this.setDatePickerHandlers = function (handler) {
+    setDatePickerHandlers(handler) {
         this.handlers.datePickerHandlers.push(handler);
     };
 
-    this.setShopListHandlers = function(handler) {
+    setShopListHandlers(handler) {
         this.handlers.shopListLoadingDataHandlers.push(handler);
     };
 
-    this.setWeekMealDataHandlers = function(handler) {
+    setWeekMealDataHandlers(handler) {
         this.handlers.weekMealsLoadingDataHandlers.push(handler);
     };
 
-    this.runDatePickerHandlers = function (startDate, endDate) {
+    runDatePickerHandlers(startDate, endDate) {
         this.handlers.datePickerHandlers.forEach(function (handler) {
             handler(startDate, endDate);
         })
     };
 
-    this.runShopListHandlers = function (date, duration) {
+    runShopListHandlers(date, duration) {
         this.handlers.shopListLoadingDataHandlers.forEach(function (handler) {
             handler(date, duration);
         })
     };
 
-    this.runWeekMealsHandlers = function (startDate) {
+    runWeekMealsHandlers(startDate) {
         this.handlers.weekMealsLoadingDataHandlers.forEach(function (handler) {
             handler(startDate);
         })
     };
 }
+
+Mealpler.controller('DashboardCtrl', DashboardController);
