4a36fb72db19107d6bfc559fdf34429462b55e21
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-14 14:25:31 +0200

    MP-34: extracted sorting shoplist to service

---
+++ client/mealpler/components/shoplist/shoplist.service.js
@@ -0,0 +1,38 @@
+class ShopListService {
+    constructor() {
+
+    }
+
+    extractAndSortProducts(list) {
+        return sortProducts(extractProducts(list));
+    }
+
+    extractProducts(list) {
+        const extracted = [];
+        list.map(ingestion => ingestion.list.map(meal => meal.mealList.map(food => {
+            if (!food.hasIngredients) {
+                extracted.push(food)
+            } else if (food.hasIngredients) {
+                food.list.map(product => extracted.push(product));
+            }
+        })));
+        extracted.forEach(a => {
+            a.name = a.name.trim();
+        });
+        return extracted;
+    }
+
+    sortProducts(list) {
+        const sorted = [];
+        list.forEach((a) => {
+            let thisNameIndex = sorted.findIndex(b => b.name === a.name);
+            if (thisNameIndex > -1) {
+                sorted[thisNameIndex].quantity++;
+            } else {
+                sorted.push(angular.copy(a));
+            }
+        });
+        return sorted;
+    }
+}
+Mealpler.service('ShopListService', ShopListService);
