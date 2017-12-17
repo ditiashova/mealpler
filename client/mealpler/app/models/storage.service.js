824c9313fd48819ec512c0bad885c6252f5bd809
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-18 00:08:17 +0200

    MP-34: storage service as class

--- client/mealpler/app/models/storage.service.js
+++ client/mealpler/app/models/storage.service.js
@@ -1,33 +1,35 @@
-Mealpler.service('StorageModel', StorageModel);
+class StorageService {
+    constructor () {
 
-function StorageModel () {
-    this.addItemToGroceryList = (item) => {
+    }
+
+    addItemToGroceryList(item) {
         let list = this.getStoredItem("grocery").concat(item);
         this.addFoodToStored("grocery", list);
-    };
+    }
 
-    this.updateGroceryItem = (oldItem) => {
+    updateGroceryItem(oldItem) {
         let list = this.getStoredItem("grocery");
         list.filter(a => a.name === oldItem.name)[0].quantity = oldItem.quantity;
         this.addFoodToStored("grocery", list);
-    };
+    }
 
-    this.deleteGroceryItem = (itemToDelete) => {
+    deleteGroceryItem(itemToDelete) {
         let list = this.getStoredItem("grocery");
         let i = list.findIndex(a => a.name === itemToDelete.name && a.quantity === itemToDelete.quantity);
         list.splice(i, 1);
         this.addFoodToStored("grocery", list);
-    };
+    }
 
-    this.deleteGrocery = () => {
+    deleteGrocery() {
         localStorage.removeItem("grocery");
     };
 
-    this.addFoodToStored = (name, content) => {
+    addFoodToStored(name, content) {
         localStorage.setItem(name, JSON.stringify(content));
     };
 
-    this.getStoredItem = (name) => {
+    getStoredItem(name) {
         let storedItem = [];
         try {
             storedItem = JSON.parse(localStorage.getItem(name));
@@ -37,3 +39,5 @@ function StorageModel () {
         return storedItem != null ? storedItem : [];
     };
 }
+
+Mealpler.service('StorageService', StorageService);
