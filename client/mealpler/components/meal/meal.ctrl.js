b9e82c0babba5aa94a688dd4fc7ac1c4cfa2744a
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-18 14:03:46 +0200

    MP-34: meal controller as class

--- client/mealpler/components/meal/meal.ctrl.js
+++ client/mealpler/components/meal/meal.ctrl.js
@@ -1,11 +1,12 @@
-Mealpler.controller('MealCtrl', MealController);
+class MealController {
+    constructor ($scope, StorageService, openModal, $document, MealService) {
+        Object.assign(this, {$scope, StorageService, openModal, $document, MealService});
+        this.parentDivForMealModals = angular.element($document[0].querySelector('.modal-parent'));
+    }
 
-function MealController ($scope, MealModel, StorageModel, openModal, $document, MealService) {
-    const parentDivForMealModals = angular.element($document[0].querySelector('.modal-parent'));
-
-    this.addNewItems = (type, newItems, forMeal, forDay) => {
+    addNewItems(type, newItems, forMeal, forDay) {
         const formattedDay = moment(forDay).format("YYYY-M-D"); //in case nonformatted day was passed
-        let oldMealList = angular.copy(MealService.findMealList(formattedDay).filter(a => a.mealName === forMeal)[0]);
+        let oldMealList = angular.copy(this.MealService.findMealList(formattedDay).filter(a => a.mealName === forMeal)[0]);
         if (type === 'list') {
             oldMealList.mealList = oldMealList.mealList.concat(newItems.list);
         } else if (type === 'recipe') {
@@ -13,16 +14,16 @@ function MealController ($scope, MealModel, StorageModel, openModal, $document, 
         } else if (type === 'stored') {
             oldMealList.mealList = oldMealList.mealList.concat(newItems.mealList);
         }
-        MealService.saveMealInfo(oldMealList,formattedDay);
+        this.MealService.saveMealInfo(oldMealList,formattedDay);
     };
 
-    this.pasteFood = (name, forMeal, forDay) => {
-        let storedOld = StorageModel.getStoredItem(name);
+    pasteFood(name, forMeal, forDay) {
+        let storedOld = this.StorageService.getStoredItem(name);
         this.addNewItems('stored', storedOld, forMeal, forDay);
     };
 
-    this.addNewMeal = (mealName, date) => {
-        const templatePath = 'scripts/components/modals/meal/add/modal.tmpl.html';
+    addNewMeal(mealName, date) {
+        const templatePath = 'scripts/components/modals/meal/add/add.tmpl.html';
         const newMealCtrl = ($scope, $uibModalInstance) => {
             this.meal = mealName;
             this.date = date.dateObj.format("YYYY-M-D");
@@ -33,11 +34,11 @@ function MealController ($scope, MealModel, StorageModel, openModal, $document, 
             this.cancel = function () {
                 $uibModalInstance.dismiss('cancel');
             }};
-        openModal.open(templatePath, parentDivForMealModals, $scope, newMealCtrl);
+        this.openModal.open(templatePath, this.parentDivForMealModals, this.$scope, newMealCtrl);
     };
 
-    this.deleteOldMeal = (mealName, date) => {
-        const templatePath = 'scripts/components/modals/meal/delete/modal.tmpl.html';
+    deleteOldMeal(mealName, date) {
+        const templatePath = 'scripts/components/modals/meal/delete/delete.tmpl.html';
         const deleteMealCtrl = ($scope, $uibModalInstance) => {
             this.meal = mealName;
             this.date = date.dateObj.format("YYYY-M-D");
@@ -48,10 +49,12 @@ function MealController ($scope, MealModel, StorageModel, openModal, $document, 
             this.cancel = function () {
                 $uibModalInstance.dismiss('cancel');
             }};
-        openModal.open(templatePath, parentDivForMealModals, $scope, deleteMealCtrl);
+        this.openModal.open(templatePath, this.parentDivForMealModals, this.$scope, deleteMealCtrl);
     };
 
-    this.confirmMealDelete = (meal, date) => {
-        MealService.deleteMeal(meal, date);
+    confirmMealDelete(meal, date) {
+        this.MealService.deleteMeal(meal, date);
     };
 }
+
+Mealpler.controller('MealCtrl', MealController);
