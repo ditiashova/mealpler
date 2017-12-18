fff0474eb1cf6b5a984bcd9e85a832adbf2fce1f
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-18 15:32:54 +0200

    MP-34: name fixes

--- client/mealpler/components/ingredient/ingredient.service.js
+++ client/mealpler/components/ingredient/ingredient.service.js
@@ -1,15 +1,15 @@
 class IngredientService {
-    constructor(MealModel) {
-        Object.assign(this, {MealModel})
+    constructor(MealService) {
+        Object.assign(this, {MealService})
     }
 
     deleteIngredient(item, recipeName, mealName, date) {
         const storedDayName = date.dateObj.format("YYYY-M-D");
-        const availableItem = this.MealModel.getMealsList(storedDayName);
+        const availableItem = this.MealService.getMealsList(storedDayName);
         const currentRecipe = availableItem.mealsList.find(b => b.mealName === mealName).mealList.find(b => b.name === recipeName && b.hasIngredients);
         const i = currentRecipe.list.findIndex(a => a.name === item.name);
         currentRecipe.list.splice(i, 1);
-        this.MealModel.updateMealsList(storedDayName, availableItem);
+        this.MealService.updateMealsList(storedDayName, availableItem);
     };
 }
 
