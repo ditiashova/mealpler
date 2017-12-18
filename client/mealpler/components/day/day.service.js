bc7a4c11469515f69d45e41faacd1af9b20e6d5b
Inna Ditiashova <ditiashova.inna@gmail.com>
2017-12-18 18:37:35 +0200

    MP-34: fixed service name

--- client/mealpler/components/day/day.service.js
+++ client/mealpler/components/day/day.service.js
@@ -1,13 +1,13 @@
 class DayService {
-    constructor(MealModel, StorageService, DayModel) {
-        Object.assign(this, {MealModel, StorageService, DayModel});
+    constructor(MealService, StorageService, DayModel) {
+        Object.assign(this, {MealService, StorageService, DayModel});
     }
 
     pasteMenuForDay(forDay) {
         const stored = this.StorageService.getStoredItem("menu");
         const day = this.DayModel.createNewDay(forDay);
         stored.forEach(a => day.mealsList.push(a));
-        this.MealModel.updateMealsList(forDay.format("YYYY-M-D"), day);
+        this.MealService.updateMealsList(forDay.format("YYYY-M-D"), day);
     }
 }
 Mealpler.service('DayService', DayService);
