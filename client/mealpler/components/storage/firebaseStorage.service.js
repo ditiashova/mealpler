756e489a3c8a51cf916bc6b171c1d577074cb67e
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-01-15 16:34:59 +0200

    MP-42: created FirebaseStorageService to manage data from direbase

---
+++ client/mealpler/components/storage/firebaseStorage.service.js
@@ -0,0 +1,95 @@
+class FirebaseStorageService {
+    constructor () {
+
+    }
+
+
+    saveUserPlannerData(date, data, id) {
+        if (id) {
+            firebase.database().ref('users/' + id + '/meals/' + date).set(data);
+        }  else {
+            localStorage.setItem(date, JSON.stringify(data));
+        }
+    }
+
+    getAllDatesMealsList(id) {
+        let all = [];
+        return new Promise((resolve, reject) => {
+            firebase.database().ref('users/' + id + '/meals').on("value", (data) => {
+                all = data.val();
+                resolve(all);
+            }, function (errorObject) {
+                reject(errorObject);
+                console.log("The read failed: " + errorObject.code);
+            });
+        })
+    }
+
+    getMealsList(forDate, id) {
+        let all = [];
+        if (id) {
+            return new Promise((resolve, reject) => {
+                firebase.database().ref('users/' + id + '/meals/' + forDate).on("value", (data) => {
+                    all = data.val();
+                    resolve(all);
+                }, function (errorObject) {
+                    reject(errorObject);
+                    console.log("The read failed: " + errorObject.code);
+                });
+            })
+        } else {
+            try {
+                all = JSON.parse(localStorage.getItem(forDate));
+            } catch (error) {
+                console.log(error);
+            }
+            return all;
+        }
+    }
+
+    getAllMealsForUser(id) {
+        return new Promise((resolve, reject) => {
+            firebase.database().ref('users/' + id + '/meals').on("value", (data) => {
+                const all = data.val();
+                resolve(all);
+            }, function (errorObject) {
+                reject(errorObject);
+                console.log("The read failed: " + errorObject.code);
+            });
+        });
+    }
+
+    getSingleDateMealsList(date, id) {
+        let all = [];
+        return new Promise((resolve, reject) => {
+            firebase.database().ref('users/' + id + '/meals/' + date).on("value", (data) => {
+                all = data.val();
+                resolve(all);
+            }, function (errorObject) {
+                reject(errorObject);
+                console.log("The read failed: " + errorObject.code);
+            });
+        })
+    }
+
+    setSingleDateMealsList(date, data, id) {
+        firebase.database().ref('users/' + id + '/meals/' + date).set(data);
+    }
+
+
+    addFoodToLocalStore(name, content) {
+        localStorage.setItem(name, JSON.stringify(content));
+    };
+
+    getLocalyStoredItem(name) {
+        let storedItem = [];
+        try {
+            storedItem = JSON.parse(localStorage.getItem(name));
+        } catch (error) {
+            console.log(error);
+        }
+        return storedItem != null ? storedItem : [];
+    };
+}
+
+Mealpler.service('FirebaseStorageService', FirebaseStorageService);
