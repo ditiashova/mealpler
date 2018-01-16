91570675ff9e3f48150c8aa1d5f758c3c73e50c6
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-01-16 19:16:11 +0200

    MP-42: major edits for pasteFood to make it async

--- client/mealpler/components/meal/meal.ctrl.js
+++ client/mealpler/components/meal/meal.ctrl.js
@@ -1,6 +1,6 @@
 class MealController {
-    constructor ($scope, StorageService, openModal, $document, MealService, Auth) {
-        Object.assign(this, {$scope, StorageService, openModal, $document, MealService, Auth});
+    constructor ($scope, LocalStorageService, openModal, $document, MealService, Auth) {
+        Object.assign(this, {$scope, LocalStorageService, openModal, $document, MealService, Auth});
         this.parentDivForMealModals = angular.element($document[0].querySelector('.modal-parent'));
         this.Auth.$onAuthStateChanged((firebaseUserData) => {
             const userIsLogged = !!firebaseUserData;
@@ -10,22 +10,11 @@ class MealController {
         });
     }
 
-    addNewItems(type, newItems, forMeal, forDay) {
-        const formattedDay = moment(forDay).format("YYYY-M-D"); //in case nonformatted day was passed
-        let oldMealList = angular.copy(this.MealService.findMealList(formattedDay).find(a => a.mealName === forMeal));
-        if (type === 'list') {
-            oldMealList.dishesList = oldMealList.dishesList.concat(newItems.productsList);
-        } else if (type === 'recipe') {
-            oldMealList.dishesList.push(newItems);
-        } else if (type === 'stored') {
-            oldMealList.dishesList = oldMealList.dishesList.concat(newItems.dishesList);
-        }
-        this.MealService.saveMealInfo(oldMealList,formattedDay);
-    };
-
-    pasteFood(name, forMeal, forDay) {
-        let storedOld = this.StorageService.getLocalyStoredItem(name);
-        this.addNewItems('stored', storedOld, forMeal, forDay);
+    pasteFood(name, mealNo, forDay) {
+        let storedOld = this.LocalStorageService.getLocalStorageData(name);
+        return new Promise((resolve) => {
+            resolve(this.MealService.updateMealInfo(storedOld, forDay, this.userPlannerId, 'stored', mealNo));
+        })
     };
 
     openModalAddNewMeal(mealNo, date) {
