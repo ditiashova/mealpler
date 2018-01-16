5ef1022efd1e23e8b8e1fb0c9a8134df4c24dc68
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-01-16 13:53:24 +0200

    MP-42: added week service and cleaned code

--- client/mealpler/components/week/week.ctrl.js
+++ client/mealpler/components/week/week.ctrl.js
@@ -1,7 +1,6 @@
 class WeekController {
-    constructor($scope, MealModel, DayModel, MealService, Auth) {
-        Object.assign(this, {$scope, MealModel, DayModel, MealService, Auth});
-        PNotify.prototype.options.delay = 2000;
+    constructor($scope, MealModel, DayModel, MealService, Auth, WeekService) {
+        Object.assign(this, {$scope, MealModel, DayModel, MealService, Auth, WeekService});
         this.todayFullDate = moment().format('YYYY-M-D');
         this.weekDuration = 7;
 
@@ -19,40 +18,22 @@ class WeekController {
 
     init(forDate) {
         this._setNewWeekStart(forDate);
-        //this._calculateWeekRange(this.weekStartDate);
         this._setWeekFirstAndLastDays(forDate);
         this._loadMealsDataForWeekRange();
     }
 
-    _getWeekStart(date) {
-        return moment(date).startOf('week');
-    };
-
     _setNewWeekStart(date) {
-        this.weekStartDate = this._getWeekStart(date);
-    }
-
-    _calculateWeekRange(firstDay) {
-        this.weekDaysFoodInfo = [];
-
-        for (let i = 0; i < this.weekDuration; i++) {
-            const newDay = this.DayModel.createNewDay(moment(firstDay).add(i, 'day'), i);
-            this.weekDaysFoodInfo.push(newDay);
-        }
+        this.weekStartDate = this.WeekService._getWeekStart(date);
     }
 
     _setWeekFirstAndLastDays(date) {
-        this.weekFirstDay = this._getWeekStart(date);
-        this.weekLastDay = this._getWeekStart(date).add(this.weekDuration, 'day');
+        this.weekFirstDay = this.WeekService._getWeekStart(date);
+        this.weekLastDay = this.WeekService._getWeekEnd(date, this.weekDuration);
     }
 
     _loadMealsDataForWeekRange() {
         this.MealService.findDateRangeMealList(this.weekFirstDay, this.weekDuration, this.userPlannerId).then((response) => {
             this.weekDaysFoodInfo = angular.copy(response);
-            this.weekDaysFoodInfo.map(day => {
-                //day.mealsList = angular.copy(storedMeals.find(a => a.fullDate === day.fullDate).mealsList);
-                day.mealsList.forEach(a => a.hasMeals = a.dishesList.length > 0);
-            });
             this.$scope.$apply();
         }, (error) => {
             console.log(error);
