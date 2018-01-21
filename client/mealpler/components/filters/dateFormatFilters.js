31444c7cac547393b3cddfcd0b059d4af2ef8f41
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-01-21 15:54:48 +0200

    MP-45: rewrote filters with arrow functions

--- client/mealpler/components/filters/dateFormatFilters.js
+++ client/mealpler/components/filters/dateFormatFilters.js
@@ -1,23 +1,17 @@
 Mealpler.
     filter('monthDay', () => {
-        function monthDaythFilter(date) {
+        return date => {
             if (date) return date.format("MMMM Do");
         }
-        monthDaythFilter.$stateful = true;
-        return monthDaythFilter;
     }).
     filter('fullDate', () => {
-        function fullDateFilter(date) {
+        return date => {
             if (date) return date.format("YYYY-M-D");
         }
-        fullDateFilter.$stateful = true;
-        return fullDateFilter;
     }).
     filter('dayName', () => {
-        function dayNameFilter(date) {
+        return date => {
             if (date) return date.format("dddd");
         }
-        dayNameFilter.$stateful = true;
-        return dayNameFilter;
     });
 
