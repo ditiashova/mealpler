0fa28f3efe9a128a9e11f6d7696dc6cf34bd53cf
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-01-28 13:07:49 +0200

    MP-48: rewrote form validation as component

--- client/mealpler/components/meal/add/list/form-validation/form-validation.directive.js
+++ client/mealpler/components/meal/add/list/form-validation/form-validation.directive.js
@@ -1,19 +1,11 @@
-Mealpler.directive('formValidation', function () {
-    const link = (scope, el, attrs, ctrl) => {
-        ctrl.form = scope.$parent.itemListForm;
-        ctrl.currFormField = ctrl.form[ctrl.inputName];
-    };
-
-    return {
-        restrict: 'E',
-        transclude: true,
-        scope: {},
-        controller: () => {},
-        controllerAs: '$ctrl',
-        bindToController: {
-            inputName: '@'
-        },
-        templateUrl: 'scripts/components/meal/add/list/form-validation/form-validation.tmpl.html',
-        link: link
-    };
+Mealpler.component('formValidation', {
+    bindings: {
+        input: '@inputName'
+    },
+    transclude: true,
+    controller: () => {},
+    require: {
+        form: '^form'
+    },
+    templateUrl: 'scripts/components/meal/add/list/form-validation/form-validation.tmpl.html',
 });
