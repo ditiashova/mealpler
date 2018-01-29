b577b2a95e4ff0c77daf97ed42efc604ae576026
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-01-29 15:14:53 +0200

    MP-48: refactored dish directive as component

--- client/mealpler/components/dish/dish.ctrl.js
+++ client/mealpler/components/dish/dish.ctrl.js
@@ -1,11 +1,26 @@
 class DishCtrl {
-    constructor(copy) {
-        Object.assign(this, {copy})
+    constructor(copy, notify, DishService, IngredientService) {
+        Object.assign(this, {copy, notify, DishService, IngredientService})
     }
 
     copySingleDish(content) {
         this.copy.copyFood('meal', content);
     }
+
+    deleteDish(item, mealNo, day) {
+        const userId = this.MainCtrl.uid;
+        this.DishService.deleteDish(item, mealNo, day.date, userId)
+            .then(() => this.MainCtrl.runDatabaseHandlers(userId))
+            .then(() => this.notify.show('Food has been deleted.', 'delete'))
+            .catch(console.log);
+    }
+
+    deleteIngredient(ingredient, itemName, mealNo, day) {
+        const userId = this.MainCtrl.uid;
+        this.IngredientService.deleteIngredient(ingredient, itemName, mealNo, day.date, userId)
+            .then(() => this.MainCtrl.runDatabaseHandlers(userId))
+            .catch(console.log);
+    };
 }
 
 Mealpler.controller('DishCtrl', DishCtrl);
--- client/mealpler/components/dish/dish.directive.js
+++ client/mealpler/components/dish/dish.directive.js
@@ -1,35 +1,13 @@
-Mealpler.directive('dishBlock', function (DishService, IngredientService, notify) {
-    const link = (scope, el, attrs, [MealCtrl, DayCtrl, WeekCtrl, MainCtrl]) => {
-        const DishCtrl = scope.dishCtrl;
-
-        DishCtrl.deleteDish = (item, mealNo, day) => {
-            const userId = MainCtrl.uid;
-            DishService.deleteDish(item, mealNo, day.date, userId)
-                .then(() => MainCtrl.runDatabaseHandlers(userId))
-                .then(() => notify.show('Food has been deleted.', 'delete'))
-                .catch(console.log);
-        };
-
-        DishCtrl.deleteIngredient = (ingredient, itemName, mealNo, day) => {
-            const userId = MainCtrl.uid;
-            IngredientService.deleteIngredient(ingredient, itemName, mealNo, day.date, userId)
-                .then(() => MainCtrl.runDatabaseHandlers(userId))
-                .catch(console.log);
-        };
-    };
-
-    return {
-        restrict: 'E',
-        transclude: true,
-        scope: {
-            dish: '=',
-            mealNo: '=',
-            day: '='
-        },
-        controller: 'DishCtrl',
-        controllerAs: 'dishCtrl',
-        require: ['^^mealManager', '^^dayManager', '^^weekManager', '^^mainBlock'],
-        templateUrl: 'scripts/components/dish/dish.tmpl.html',
-        link: link
-    };
+Mealpler.component('dishBlock', {
+    bindings: {
+        dish: '<',
+        mealNo: '<',
+        day: '<'
+    },
+    require: {
+        MainCtrl: '^^mainBlock'
+    },
+    transclude: true,
+    controller: 'DishCtrl',
+    templateUrl: 'scripts/components/dish/dish.tmpl.html',
 });
