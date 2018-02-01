f96e0d87b1332992e4a2c301fe63aed28b468da0
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-02-01 18:45:41 +0200

    MP-52: cleaned and fixed shoplist

--- client/mealpler/components/shoplist/shoplist.component.js
+++ client/mealpler/components/shoplist/shoplist.component.js
@@ -4,39 +4,16 @@ Mealpler.component('shopList', {
     },
     require: {},
     transclude: true,
-    controller: function ($scope, WeekService, ShopListService, $timeout) {
+    controller: function ($scope, WeekService, ShopListService) {
         this.title = Mealpler.titles.shopList;
-        this.$onInit = () => {
+
+        this.$onChanges = () => {
             this.shoplist = ShopListService.extractAndSortProducts(this.week);
-            //this.MainCtrl.addDatabaseHandlers((data, date, duration) => this.init(date, duration, data));
-
-            //this.Dashboard.addShopListHandlers((date, duration) => this.init(date, duration));
-
-            //this.init(this.startDate);
-            this.closeShopList = () => {
-                $scope.$emit(EventType.SHOPLIST_CLOSED);
-                //this.MainCtrl.toggleShopListState();
-            }
         };
 
-        /*this.init = (start = this.startDate, duration = this.rangeDuration, data) => {
-            if (data || data === null) {
-                //data could be null if there is no data for user, but undefined data means no data from database
-                const storedItems = WeekService.organizeDataForWeek(start, duration, data);
-                $timeout(() => {
-                    return Promise.resolve(this.componentsList = ShopListService.extractAndSortProducts(storedItems));
-                });
-            } else {
-                return WeekService.findDateRangeMealList(start, duration).then((response) => {
-                    const storedItems = angular.copy(response);
-                    $timeout(() => {
-                        this.componentsList = ShopListService.extractAndSortProducts(storedItems);
-                    })
-                });
-            }
-        };*/
-
-
+        this.closeShopList = () => {
+            $scope.$emit(EventType.SHOPLIST_CLOSED);
+        }
     },
     templateUrl: 'scripts/components/shoplist/shopList.tmpl.html',
 });
