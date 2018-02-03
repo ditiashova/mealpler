b5149bb4f045732407336e31b2873f6a83e3149f
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-02-03 17:38:18 +0200

    MP-54: refactored auth service

--- client/mealpler/components/auth/auth.service.js
+++ client/mealpler/components/auth/auth.service.js
@@ -1,36 +1,7 @@
 class AuthService {
     constructor ($rootScope, FirebaseAuth, FirebaseData, LocalStorageData) {
         Object.assign(this, {$rootScope, FirebaseAuth, FirebaseData, LocalStorageData});
-        this.init();
-        this.handlers = [];
-    }
-
-    init() {
-        this.FirebaseAuth.$onAuthStateChanged((user) => {
-            this._setUserIsLogged(!!user);
-            if (user) {
-                // User is signed in.
-                this.FirebaseData.getUserProfile(user.uid)
-                    .then(data => {
-                        if (!data.val()) {
-                            this._registerNewUser(user);
-                        } else {
-                            //if user is logged and it's not a new user, it's better to keep localStorages clean
-                            this.LocalStorageData.removeLocalStorageData('Mealpler');
-                        }
-                        this._setUser(user);
-                    });
-            } else {
-                this._setUser();
-            }
-        });
-    }
-
-    _registerNewUser(user) {
-        const localData = this.LocalStorageData.getLocalStorageData("Mealpler");
-        this.FirebaseData.registerNewUserInDatabase(user, localData)
-            .then(() => this.LocalStorageData.removeLocalStorageData("Mealpler"))
-            .catch((e) => console.log('Failed to register new user due to: '+ e.message));
+        this._init();
     }
 
     isLogged() {
@@ -60,6 +31,34 @@ class AuthService {
 
     _setUserIsLogged(status) {
         this.userIsLogged = status;
+    }
+
+    _registerNewUser(user) {
+        const localData = this.LocalStorageData.getLocalStorageData("Mealpler");
+        this.FirebaseData.registerNewUserInDatabase(user, localData)
+            .then(() => this.LocalStorageData.removeLocalStorageData("Mealpler"))
+            .catch((e) => console.log('Failed to register new user due to: '+ e.message));
+    }
+
+    _init() {
+        this.FirebaseAuth.$onAuthStateChanged((user) => {
+            this._setUserIsLogged(!!user);
+            if (user) {
+                // User is signed in.
+                this.FirebaseData.getUserProfile(user.uid)
+                    .then(data => {
+                        if (!data.val()) {
+                            this._registerNewUser(user);
+                        } else {
+                            //if user is logged and it's not a new user, it's better to keep localStorages clean
+                            this.LocalStorageData.removeLocalStorageData('Mealpler');
+                        }
+                        this._setUser(user);
+                    });
+            } else {
+                this._setUser();
+            }
+        });
     }
 }
 
