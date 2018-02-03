7fa4483a786ce7929a0f5dc54909d2083317bb6c
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-02-03 22:29:16 +0200

    MP-54: cleaned dish ctrl

--- client/mealpler/components/dish/dish.ctrl.js
+++ client/mealpler/components/dish/dish.ctrl.js
@@ -8,17 +8,13 @@ class DishCtrl {
     }
 
     deleteDish(item, mealType, date) {
-        //const userId = this.MainCtrl.uid;
         this.DishService.deleteDish(item, mealType, date)
-            //.then(() => this.MainCtrl.runDatabaseHandlers(userId))
             .then(() => this.NotifyService.show('Food has been deleted.', 'delete'))
             .catch((e) => console.log('Dish deleting failed due to: '+ e.message));
     }
 
     deleteIngredient(ingredient, itemName, mealType, date) {
-        //const userId = this.MainCtrl.uid;
         this.IngredientService.deleteIngredient(ingredient, itemName, mealType, date)
-            //.then(() => this.MainCtrl.runDatabaseHandlers(userId))
             .catch((e) => console.log('Ingredient deleting failed due to: '+ e.message));
     };
 }
