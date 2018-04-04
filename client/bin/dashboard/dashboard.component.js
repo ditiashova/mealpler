9d566f512c4db17de5226241856b62783f884608
Inna Ditiashova <ditiashova.inna@gmail.com>
2018-04-04 10:14:24 +0300

    MP-56: removed bin folder

--- client/bin/dashboard/dashboard.component.js
+++
@@ -1,7 +0,0 @@
-Mealpler.component('dashboard', {
-    bindings: {},
-    require: {},
-    transclude: true,
-    controller: 'DashboardCtrl',
-    templateUrl: 'scripts/components/dashboard/dashboard.html',
-});
